7f24429980862de19d0d1d0d930202d945f52908
Jeff Ashton <JeffAshton@users.noreply.github.com>
2019-09-18 10:52:27 -0400

    Adding another test case

--- tests/kibana_discover_test.py
+++ tests/kibana_discover_test.py
@@ -292,11 +292,11 @@ def test_kibana_discover_url_with_int_query_key():
             'kibana_discover_url': 'http://kibana:5601/#/discover',
             'kibana_discover_index_pattern_id': 'logs-*',
             'timestamp_field': 'timestamp',
-            'query_key': 'response'
+            'query_key': 'geo.dest'
         },
         match={
             'timestamp': '2019-09-01T00:30:00Z',
-            'response': 200
+            'geo.dest': 200
         }
     )
     expectedUrl = (
@@ -312,13 +312,13 @@ def test_kibana_discover_url_with_int_query_key():
         + '&_a=%28'  # app start
         + 'columns%3A%21%28_source%29%2C'
         + 'filters%3A%21%28'  # filters start
-        + '%28'  # filters end
+        + '%28'  # filter start
         + '%27%24state%27%3A%28store%3AappState%29%2C'
         + 'meta%3A%28'  # meta start
         + 'alias%3A%21n%2C'
         + 'disabled%3A%21f%2C'
         + 'index%3A%27logs-%2A%27%2C'
-        + 'key%3Aresponse%2C'
+        + 'key%3Ageo.dest%2C'
         + 'negate%3A%21f%2C'
         + 'params%3A%28query%3A200%2C'  # params start
         + 'type%3Aphrase'
@@ -328,10 +328,10 @@ def test_kibana_discover_url_with_int_query_key():
         + '%29%2C'  # meta end
         + 'query%3A%28'  # query start
         + 'match%3A%28'  # match start
-        + 'response%3A%28'  # reponse start
+        + 'geo.dest%3A%28'  # reponse start
         + 'query%3A200%2C'
         + 'type%3Aphrase'
-        + '%29'  # response end
+        + '%29'  # geo.dest end
         + '%29'  # match end
         + '%29'  # query end
         + '%29'  # filter end
@@ -350,11 +350,11 @@ def test_kibana_discover_url_with_str_query_key():
             'kibana_discover_url': 'http://kibana:5601/#/discover',
             'kibana_discover_index_pattern_id': 'logs-*',
             'timestamp_field': 'timestamp',
-            'query_key': 'response'
+            'query_key': 'geo.dest'
         },
         match={
             'timestamp': '2019-09-01T00:30:00Z',
-            'response': 'ok'
+            'geo.dest': 'ok'
         }
     )
     expectedUrl = (
@@ -370,13 +370,13 @@ def test_kibana_discover_url_with_str_query_key():
         + '&_a=%28'  # app start
         + 'columns%3A%21%28_source%29%2C'
         + 'filters%3A%21%28'  # filters start
-        + '%28'  # filters end
+        + '%28'  # filter start
         + '%27%24state%27%3A%28store%3AappState%29%2C'
         + 'meta%3A%28'  # meta start
         + 'alias%3A%21n%2C'
         + 'disabled%3A%21f%2C'
         + 'index%3A%27logs-%2A%27%2C'
-        + 'key%3Aresponse%2C'
+        + 'key%3Ageo.dest%2C'
         + 'negate%3A%21f%2C'
         + 'params%3A%28query%3Aok%2C'  # params start
         + 'type%3Aphrase'
@@ -386,13 +386,101 @@ def test_kibana_discover_url_with_str_query_key():
         + '%29%2C'  # meta end
         + 'query%3A%28'  # query start
         + 'match%3A%28'  # match start
-        + 'response%3A%28'  # reponse start
+        + 'geo.dest%3A%28'  # reponse start
         + 'query%3Aok%2C'
         + 'type%3Aphrase'
-        + '%29'  # response end
+        + '%29'  # geo.dest end
         + '%29'  # match end
         + '%29'  # query end
         + '%29'  # filter end
+        + '%29%2C'  # filters end
+        + 'index%3A%27logs-%2A%27%2C'
+        + 'interval%3Aauto'
+        + '%29'  # app end
+    )
+    assert url == expectedUrl
+
+
+def test_kibana_discover_url_with_compound_query_key():
+    url = kibana_discover_url(
+        rule={
+            'use_kibana_discover': '6.8',
+            'kibana_discover_url': 'http://kibana:5601/#/discover',
+            'kibana_discover_index_pattern_id': 'logs-*',
+            'timestamp_field': 'timestamp',
+            'compound_query_key': ['geo.src', 'geo.dest'],
+            'query_key': 'geo.src,geo.dest'
+        },
+        match={
+            'timestamp': '2019-09-01T00:30:00Z',
+            'geo': {
+                'src': 'CA',
+                'dest': 'US'
+            }
+        }
+    )
+    expectedUrl = (
+        'http://kibana:5601/#/discover'
+        + '?_g=%28'  # global start
+        + 'refreshInterval%3A%28pause%3A%21t%2Cvalue%3A0%29%2C'
+        + 'time%3A%28'  # time start
+        + 'from%3A%272019-09-01T00%3A20%3A00Z%27%2C'
+        + 'mode%3Aabsolute%2C'
+        + 'to%3A%272019-09-01T00%3A40%3A00Z%27'
+        + '%29'  # time end
+        + '%29'  # global end
+        + '&_a=%28'  # app start
+        + 'columns%3A%21%28_source%29%2C'
+        + 'filters%3A%21%28'  # filters start
+
+        + '%28'  # geo.src filter start
+        + '%27%24state%27%3A%28store%3AappState%29%2C'
+        + 'meta%3A%28'  # meta start
+        + 'alias%3A%21n%2C'
+        + 'disabled%3A%21f%2C'
+        + 'index%3A%27logs-%2A%27%2C'
+        + 'key%3Ageo.src%2C'
+        + 'negate%3A%21f%2C'
+        + 'params%3A%28query%3ACA%2C'  # params start
+        + 'type%3Aphrase'
+        + '%29%2C'  # params end
+        + 'type%3Aphrase%2C'
+        + 'value%3ACA'
+        + '%29%2C'  # meta end
+        + 'query%3A%28'  # query start
+        + 'match%3A%28'  # match start
+        + 'geo.src%3A%28'  # reponse start
+        + 'query%3ACA%2C'
+        + 'type%3Aphrase'
+        + '%29'  # geo.src end
+        + '%29'  # match end
+        + '%29'  # query end
+        + '%29%2C'  # geo.src filter end
+
+        + '%28'  # geo.dest filter start
+        + '%27%24state%27%3A%28store%3AappState%29%2C'
+        + 'meta%3A%28'  # meta start
+        + 'alias%3A%21n%2C'
+        + 'disabled%3A%21f%2C'
+        + 'index%3A%27logs-%2A%27%2C'
+        + 'key%3Ageo.dest%2C'
+        + 'negate%3A%21f%2C'
+        + 'params%3A%28query%3AUS%2C'  # params start
+        + 'type%3Aphrase'
+        + '%29%2C'  # params end
+        + 'type%3Aphrase%2C'
+        + 'value%3AUS'
+        + '%29%2C'  # meta end
+        + 'query%3A%28'  # query start
+        + 'match%3A%28'  # match start
+        + 'geo.dest%3A%28'  # reponse start
+        + 'query%3AUS%2C'
+        + 'type%3Aphrase'
+        + '%29'  # geo.dest end
+        + '%29'  # match end
+        + '%29'  # query end
+        + '%29'  # geo.dest filter end
+
         + '%29%2C'  # filters end
         + 'index%3A%27logs-%2A%27%2C'
         + 'interval%3Aauto'
